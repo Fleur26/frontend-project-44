eb6e254703f6747b4bb810032174f4a90a12534f
Valeria Khokholeva <lera.hoholeva@yandex.ru>
2023-12-26 16:18:59 +0400

    add game prime

---
+++ games/brainPrime.js
@@ -0,0 +1,26 @@
+import {
+  winOrLoose, gameGreeting, takePlayerAnswer, validateAnswer,
+} from '../src/game-logic.js';
+import { getRandomNumber, isPrime } from '../src/arithmetic-logic.js';
+
+const brainPrime = () => {
+  const playerName = gameGreeting('Answer "yes" if given number is prime. Otherwise answer "no".');
+  let countRightAnswers = 3;
+  let defeatCount = 0;
+  let randomNumb = 0;
+  while (countRightAnswers > 0 && defeatCount === 0) {
+    countRightAnswers -= 1;
+    randomNumb = getRandomNumber(1, 100);
+    console.log(`Question: ${randomNumb}`);
+    const playerAnswer = takePlayerAnswer('Your answer: ');
+    let rightAnswer = isPrime(randomNumb);
+    if (rightAnswer) {
+      rightAnswer = 'yes';
+    } else {
+      rightAnswer = 'no';
+    }
+    defeatCount = validateAnswer(playerAnswer, rightAnswer);
+  }
+  winOrLoose(defeatCount, playerName);
+};
+export default brainPrime;
