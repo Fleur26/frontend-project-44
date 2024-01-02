25b753fafe7a639d52db6e4039a20c55f819452a
Valeria Khokholeva <lera.hoholeva@yandex.ru>
2024-01-02 21:41:03 +0400

    add return to cli

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,7 @@ const namedGreetings = () => {
   console.log('Welcome to the Brain Games!');
   const playerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${playerName}!`);
+  return playerName;
 };
 
 export default namedGreetings;
